c549b61472cb52e326424507823d82d3e64687c4
PC <mustafa.bahr12323@gmail.com>
2024-08-01 13:15:59 +0300

    UserModel

---
+++ lib/models/user_model.dart
@@ -0,0 +1,45 @@
+class UserModel {
+  final String name;
+  final String email;
+  final String password;
+  final String uid;
+  final String image;
+  final List<dynamic> followers;
+  final List<dynamic> following;
+
+  UserModel({
+    required this.name,
+    required this.email,
+    required this.password,
+    required this.uid,
+    required this.image,
+    required this.followers,
+    required this.following,
+  });
+
+  // Convert UserModel to a map
+  Map<String, dynamic> toMap() {
+    return {
+      'name': name,
+      'email': email,
+      'password': password,
+      'uid': uid,
+      'image': image,
+      'followers': followers,
+      'following': following,
+    };
+  }
+
+  // Create UserModel from a map
+  factory UserModel.fromMap(Map<String, dynamic> map) {
+    return UserModel(
+      name: map['name'] ?? '',
+      email: map['email'] ?? '',
+      password: map['password'] ?? '',
+      uid: map['uid'] ?? '',
+      image: map['image'] ?? '',
+      followers: List<dynamic>.from(map['followers'] ?? []),
+      following: List<dynamic>.from(map['following'] ?? []),
+    );
+  }
+}
